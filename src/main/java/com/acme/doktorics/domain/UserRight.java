2359977ee1968cc0d29d9df0f61736778f226abf
doktoric <doktoricsrichard@gmail.com>
2013-01-10 14:10:16 +0100

    userroles

--- src/main/java/com/acme/doktorics/domain/UserRight.java
+++ src/main/java/com/acme/doktorics/domain/UserRight.java
@@ -2,6 +2,26 @@ package com.acme.doktorics.domain;
 
 
 
+
 public enum UserRight {
-    ADMIN,USER;
+    
+    
+    ADMIN_ROLE("ADMIN_ROLE"),USER_ROLE("USER_ROLE");
+    
+    private String right;
+    
+    private UserRight(String right)
+    {
+        this.right=right;
+    }
+
+    public String getRight() {
+        return right;
+    }
+
+    public void setRight(String right) {
+        this.right = right;
+    }
+    
+    
 }
