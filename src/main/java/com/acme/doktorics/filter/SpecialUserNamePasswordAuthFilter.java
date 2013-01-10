be2c9b185670ac145c5b4b28945ed22bc6765a60
doktoric <doktoricsrichard@gmail.com>
2013-01-10 14:11:17 +0100

    filter to login

---
+++ src/main/java/com/acme/doktorics/filter/SpecialUserNamePasswordAuthFilter.java
@@ -0,0 +1,25 @@
+package com.acme.doktorics.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SpecialUserNamePasswordAuthFilter extends UsernamePasswordAuthenticationFilter {
+
+    
+    private final Logger logger = LoggerFactory.getLogger(SpecialUserNamePasswordAuthFilter.class);
+    
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        logger.info("login attempt from:" + request.getRemoteAddr());
+        Authentication authentication = super.attemptAuthentication(request, response);
+        return authentication;
+    }
+    
+    
+}
