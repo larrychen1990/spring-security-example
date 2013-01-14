5c95edebf2eba5236682cf2e1a53e25906bce04a
doktoric <doktoricsrichard@gmail.com>
2013-01-14 23:28:05 +0100

    new finders

--- src/main/java/com/acme/doktorics/service/SimpleRoleService.java
+++ src/main/java/com/acme/doktorics/service/SimpleRoleService.java
@@ -11,47 +11,80 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.acme.doktorics.dao.RoleDAO;
 import com.acme.doktorics.domain.Role;
+import com.acme.doktorics.domain.Sorting;
 
 @Service
 @Transactional
 public class SimpleRoleService implements RoleService {
 
-    @Autowired
-    private RoleDAO roleDao;
-    protected static final Logger logger = LoggerFactory.getLogger(SimpleRoleService.class);
+	@Autowired
+	private RoleDAO roleDao;
+	protected static final Logger logger = LoggerFactory
+			.getLogger(SimpleRoleService.class);
 
-    @Override
-    public List<Role> findAll() {
-        List<Role> roles = roleDao.findAll();
-        if (roles == null) {
-            roles = new ArrayList<Role>();
-        }
-        return roles;
-    }
+	@Override
+	public List<Role> findAll() {
+		List<Role> roles = roleDao.findAll();
+		if (roles == null) {
+			roles = new ArrayList<Role>();
+		}
+		return roles;
+	}
 
-    @Override
-    public void delete(String id) {
-        Role role = roleDao.findOne(Long.parseLong(id));
-        roleDao.delete(role);
+	@Override
+	public void delete(String id) {
+		Role role = roleDao.findOne(Long.parseLong(id));
+		roleDao.delete(role);
 
-    }
+	}
 
-    @Override
-    public void save(Role role) {
-        roleDao.save(role);
+	@Override
+	public void save(Role role) {
+		roleDao.save(role);
 
-    }
+	}
 
-    @Override
-    public Role findByName(String name) {
-        Role role = roleDao.findElementByName("name", name);
-        return role;
-    }
+	@Override
+	public Role findByName(String name) {
+		Role role = roleDao.findElementByName("name", name);
+		return role;
+	}
 
-    @Override
-    public void update(Role role) {
-        roleDao.update(role);
-        
-    }
+	@Override
+	public void update(Role role) {
+		roleDao.update(role);
 
+	}
+
+	@Override
+	public Role findMostSignificantRole() {
+		Role result = null;
+		List<Role> roles = findAll();
+		for (Role role : roles) {
+			if (role.getName().equals( "ROLE_ADMIN")) {
+				result = role;
+			}
+		}
+		return result;
+
+	}
+
+	@Override
+	public List<Role> findAllRolebyID() {
+		List<Role> roles = (List<Role>) roleDao.findAllBySorting("id",
+				Sorting.ASC);
+		return roles;
+	}
+
+	@Override
+	public Role findMostWeakestRole() {
+		Role result = null;
+		List<Role> roles = findAll();
+		for (Role role : roles) {
+			if (role.getName().equals("ROLE_USER")) {
+				result = role;
+			}
+		}
+		return result;
+	}
 }
